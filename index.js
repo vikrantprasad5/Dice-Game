a0314eb2c3c9261d6115faf707bde24815dca260
vikrantprasad5 <66894598+vikrantprasad5@users.noreply.github.com>
2020-07-15 22:20:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var randomNumber2=Math.floor(Math.random()*6+1);
 document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 if(randomNumber1===randomNumber2)
 {
-    document.querySelector("h1").innerHTML="🎌 Draw! 🎌";
+    document.querySelector("h1").innerHTML="🎌 Draw!";
 }
 else if(randomNumber1>randomNumber2)
 {
